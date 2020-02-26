f4a80448e379d9b5fa086695af2fe5146a604352
blackpoint <marcinw1998@gmail.com>
2020-02-26 22:15:00 +0100

    Added simple ReminderService.

--- src/main/java/com/blackpoint/axel/controller/BotController.java
+++ src/main/java/com/blackpoint/axel/controller/BotController.java
@@ -5,12 +5,16 @@ import com.blackpoint.axel.service.MessageService;
 import com.blackpoint.axel.model.TelegramMessages.Response;
 import com.blackpoint.axel.model.TelegramMessages.Update;
 import com.blackpoint.axel.service.Logger;
+import com.blackpoint.axel.service.ReminderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.HashMap;
 
 @RestController
@@ -49,7 +53,7 @@ public class BotController {
     {
         logger.displayUpdateMessageContent(update);
         try {
-            String[] message = update.getMessage().getContent().split(" ", 2);
+            String[] message = update.getMessage().getContent().split(" ", 3);
             switch(message[0]) {
                 case "/weather":
                     if (message.length < 2) {
@@ -68,6 +72,38 @@ public class BotController {
                             weatherController.getWeather(message[1])
                     );
                     return;
+
+                case "/time":
+                    Calendar now = Calendar.getInstance();
+                    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
+
+                    messageService.sendMessage(
+                            update.getMessage().getChat().getId(),
+                            "Current system time: " + simpleDateFormat.format(now.getTime())
+                            );
+                    break;
+
+                case "/reminder":
+                    if (message.length < 3) {
+                        logger.error("[ Axel ] | Wrong reminder parameters");
+                        messageService.sendMessage(
+                                update.getMessage().getChat().getId(),
+                                "Please, specify time and title of reminder, ex. '/reminder 12:34:00 Shopping'"
+                        );
+                        return;
+                    }
+                    logger.info("[ Axel ] | /reminder request detected, setting reminder for: " + message[1]);
+                    ReminderService reminderService = new ReminderService();
+                    try {
+                        reminderService.setReminder(message[1], message[2], messageService, update.getMessage().getChat().getId());
+                        messageService.sendMessage(
+                                update.getMessage().getChat().getId(),
+                                "Your reminder is set to " + message[1]);
+                    }
+                    catch (ParseException e) {
+                        logger.error("[ Axel ] | Argument parser error: " + e.getMessage());
+                    }
+                    break;
 
                 case "/help":
                     messageService.sendMessage(
---
+++ src/main/java/com/blackpoint/axel/service/ReminderService.java
@@ -0,0 +1,60 @@
+package com.blackpoint.axel.service;
+
+import org.springframework.stereotype.Service;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+@Service
+public class ReminderService {
+    static class ReminderTask extends TimerTask {
+
+        private final MessageService messageService;
+        private final long userId;
+        private final String title;
+
+        public ReminderTask(MessageService messageService, String title, long userId) {
+            this.messageService = messageService;
+            this.userId = userId;
+            this.title = title;
+        }
+
+        @Override
+        public void run() {
+            messageService.sendMessage(userId, "You have reminder! " + title);
+        }
+    }
+
+    public void setReminder(String time, String title, MessageService messageService, long userId) throws ParseException {
+        String[] timeStringSplit = time.split(":");
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, Integer.parseInt(timeStringSplit[0]));
+        calendar.set(Calendar.MINUTE, Integer.parseInt(timeStringSplit[1]));
+        calendar.set(Calendar.SECOND, Integer.parseInt(timeStringSplit[2]));
+
+        Date date = calendar.getTime();
+
+        Timer timer = new Timer();
+        timer.schedule(new ReminderTask(messageService, title, userId), date);
+    }
+
+
+    // KIND OF A TIMER FUNCTIONALITY. LEFT IT HERE FOR FUTURE UPDATES.
+//    public void setReminder(int seconds, MessageService messageService, long userId) {
+//        Calendar calendar = Calendar.getInstance();
+//        calendar.add(Calendar.SECOND, seconds);
+//
+//        Date date  = calendar.getTime();
+//
+//        Timer timer = new Timer();
+//        timer.schedule(new ReminderTask(messageService, userId), date);
+//    }
+
+
+}
